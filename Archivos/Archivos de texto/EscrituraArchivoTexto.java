7db864d07c8865832173644c1fe4c6305fcbb0fe
Luis Angel Avila Torres <luisangelavilatorres@gmail.com>
2017-05-06 07:58:00 -0500

    Archivos de texto

---
+++ Archivos/Archivos de texto/EscrituraArchivoTexto.java
@@ -0,0 +1,44 @@
+import java.io.FileWriter;
+import java.io.PrintWriter;
+
+/**
+ * @author Luis Angel Avila Torres
+ * @version 1.0
+ * 
+ * La clase FileWriter permite escribir archvivos de caracteres.
+ * El constructor de esta clase supone que la codificación de caracteres predeterminada
+ * y el tamaño predeterminado del búfer de bytes son aceptables.
+ * Para especificar estos valores, puede construir un OutputStreamWriter en un FileOutputStream.
+ * 
+ * La clase PrintWriter Imprime las representaciones formateadas de objetos en una secuencia de salida de texto (.
+ * Esta clase implementa todos los métodos de impresión encontrados en PrintStream.
+ * 
+ */
+public class EscrituraArchivoTexto{
+	public static void main(String[] args){
+        FileWriter archivo = null;
+        PrintWriter pw = null;
+        try{
+        	//Crea el archivo, si el archivo existe, escribe al final de su contenido
+            archivo = new FileWriter("D:/archivoSalida.txt", true);
+            //Crea un objeto PrintWriter, sin realizar vaciado automático de linea, con el archivo especificado.
+            pw = new PrintWriter(archivo);
+
+            for (int i = 0; i < 10; i++){
+            	//Escribe linea a linea sobre el archivo
+            	pw.println("Linea " + i);
+            }
+        }catch (Exception e){
+            System.out.println("Error: "+e);
+        }finally{
+        	try{
+        		archivo.close();
+        		if(archivo != null){
+        			System.out.println("Archivo creado correctamente");
+        		}
+        	}catch(Exception e){
+        		System.out.println("Error al cerrar el archivo: "+e);
+        	}
+        }
+    }
+}
---
+++ Archivos/Archivos de texto/LecturaArchivoTexto.java
@@ -0,0 +1,64 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+/**
+ * @author Luis Angel Avila Torres
+ * @version 1.0
+ * 
+ * La clase File es una representación abstracta de archivo y rutas de directorio
+ * 
+ * La clase FileReader permite leer archvivos de caracteres.
+ * Los constructores de esta clase suponen que la codificación de caracteres predeterminada
+ * y el tamaño predeterminado del búfer de bytes son apropiados.
+ * Para especificar estos valores, puede construir un InputStreamReader en un FileInputStream.
+ * 
+ * La clase BufferedReader permite leer el texto de una secuencia de entrada de caracteres,
+ * almacenando en búfer caracterespara proporcionar una lectura eficiente de caracteres, matrices y líneas.
+ * El tamaño del búfer puede especificarse, o puede usarse el tamaño predeterminado.
+ * El valor predeterminado es lo suficientemente grande para la mayoría de los propósitos.
+ * 
+ */
+public class LecturaArchivoTexto{
+	
+	public static void main(String[] args) {
+		//Declaración de variables
+		File archivo;
+		FileReader fr=null;
+		BufferedReader br;
+		
+		try{
+			//Almacena la ruta del archivo en la variable archivo
+			archivo = new File("D:\\archivo.txt");
+			//Crea un objeto FileReader con la ruta del archivo a leer
+			fr = new FileReader (archivo);
+			//Crea un flujo de entrada de caracteres de espacio de memoria(buffer)
+	        br = new BufferedReader(fr);
+	        
+		    // Lectura del archivo
+	        String linea;
+	        //Se recorre linea a linea el flujo de entrada de caracteres y se almacenan en la variable linea
+	        while((linea=br.readLine())!=null){
+	        	System.out.println(linea);
+	        }
+        //Se crea excepción si el sistema no encuentra el archivo
+		}catch (FileNotFoundException e) {
+			System.out.println("El sistema no puede encontrar el archivo");
+		}
+		//Se crea Excepción si el sistema encuentra cualquier otro error
+		catch (Exception e) {
+			System.out.println("Error: "+e);
+		}finally {
+			if(fr != null){
+				try{
+					fr.close();
+				}
+				//Se captura excepción si existen problemas al cerrar el archivo
+		        catch (Exception e){
+		        	System.out.println("Error al cerrar el archivo "+e);
+		        }
+			}
+		}
+	}
+}
