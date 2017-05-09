b530cef95984dc0e888dcd92c22173903055ae32
Luis Angel Avila Torres <luisangelavilatorres@gmail.com>
2017-05-09 12:35:11 -0500

    Actualización de comentarios

--- Archivos/Archivos de texto/EscrituraArchivoTexto.java
+++ Archivos/Archivos de texto/EscrituraArchivoTexto.java
@@ -5,13 +5,17 @@ import java.io.PrintWriter;
  * @author Luis Angel Avila Torres
  * @version 1.0
  * 
+ * Los Stream son una secuencia ordenada de datos con un determinado origen y destino, normalmente se utilzan en parejas (iniciador, filtro)
+ * FileWriter (iniciador): escribe texto en un fichero
+ * PrintWriter (filtro): permite convertir a texto variables y objetos para escribirlos en un Writer
+ *
  * La clase FileWriter permite escribir archvivos de caracteres.
- * El constructor de esta clase supone que la codificación de caracteres predeterminada
- * y el tamaño predeterminado del búfer de bytes son aceptables.
+ * El constructor de esta clase supone que la codificaciÃ³n de caracteres predeterminada
+ * y el tamaÃ±o predeterminado del bÃºfer de bytes son aceptables.
  * Para especificar estos valores, puede construir un OutputStreamWriter en un FileOutputStream.
  * 
- * La clase PrintWriter Imprime las representaciones formateadas de objetos en una secuencia de salida de texto (.
- * Esta clase implementa todos los métodos de impresión encontrados en PrintStream.
+ * La clase PrintWriter Imprime las representaciones formateadas de objetos en una secuencia de salida de texto.
+ * Esta clase implementa todos los mÃ©todos de impresiÃ³n encontrados en PrintStream.
  * 
  */
 public class EscrituraArchivoTexto{
@@ -19,9 +23,9 @@ public class EscrituraArchivoTexto{
         FileWriter archivo = null;
         PrintWriter pw = null;
         try{
-        	//Crea el archivo, si el archivo existe, escribe al final de su contenido
+        	//Crea el archivo, si el archivo existe, escribe al final de su contenido (Stream iniciador)
             archivo = new FileWriter("D:/archivoSalida.txt", true);
-            //Crea un objeto PrintWriter, sin realizar vaciado automático de linea, con el archivo especificado.
+            //Crea un objeto PrintWriter, sin realizar vaciado automÃ¡tico de linea, con el archivo especificado (Stream filtro).
             pw = new PrintWriter(archivo);
 
             for (int i = 0; i < 10; i++){
